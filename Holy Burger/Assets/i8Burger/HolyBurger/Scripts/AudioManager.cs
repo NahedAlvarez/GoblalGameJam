6019d58e76d62c1a6a9f0e702cfeb7e071097d94
Nahed David Alvarez Beltran <nahed.david@timba.co>
2020-02-02 14:13:28 -0500

    AudioManager

---
+++ Holy Burger/Assets/i8Burger/HolyBurger/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+	public AudioSource source;
+	public List <AudioClip> allClips = new List<AudioClip>();
+	
+	private static AudioManager instance = null;
+	// Game Instance Singleton
+	public static AudioManager InstanceA
+	{
+		get
+		{
+			return instance;
+		}
+	}
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+		instance = this;
+		DontDestroyOnLoad(this.gameObject);
+	}
+	
+ 
+	
+	
+	void Start()
+	{
+		source = GetComponent<AudioSource>();
+	}
+	
+	public void SelectAudio(string name)
+	{
+		source.PlayOneShot(AudioClipsName(name));
+	}	
+	
+	AudioClip AudioClipsName(string name)
+	{		
+		foreach(AudioClip aud in allClips)
+		{
+			if(aud.name == name)
+				return aud;
+		}
+		return null;
+	}
+}
